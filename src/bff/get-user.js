d43377bd1a76977f43dc8fc8a8bf80b3ae6dacf4
vas-alina <vasalina93@gmail.com>
2024-08-14 00:07:29 +0300

    Создание BFF

---
+++ src/bff/get-user.js
@@ -0,0 +1,6 @@
+import { getUsers } from "./get-users";
+
+export const getUser = async (loginToFind) => {
+  const users = await getUsers();
+  users.find(({ login }) => login === loginToFind);
+};
